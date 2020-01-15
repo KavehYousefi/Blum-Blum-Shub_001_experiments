761de244fffb7bf19e45932ddafa7dd562963fa9
Kaveh Yousefi <KaYousefi@o2online.de>
2020-01-15 12:01:53 +0100

    Update blum-blum-shub-random-generator.lisp

--- blum-blum-shub-random-generator.lisp
+++ blum-blum-shub-random-generator.lisp
@@ -89,7 +89,100 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; -- Test 04: Object-oriented design.                             -- ;;
+;; -- Test 04: With fungible output bits generator.                -- ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun count-one-bits (bits)
+  "Returns the number of bits in the integer-encoded BITS that equal 1."
+  (declare (integer bits))
+  (loop for bit-index from 0 below (integer-length bits)
+        when (logbitp bit-index bits) sum 1))
+
+;;; -------------------------------------------------------
+
+(defparameter +EVEN-PARITY-BIT+
+  #'(lambda (x[n+1])
+      (declare (integer x[n+1]))
+      (mod (count-one-bits x[n+1]) 2))
+  "Returns 0 if x[n+1] contains an even number of 1-bits, otherwise 0.")
+
+;;; -------------------------------------------------------
+
+(defparameter +ODD-PARITY-BIT+
+  #'(lambda (x[n+1])
+      (declare (integer x[n+1]))
+      (if (evenp (count-one-bits x[n+1])) 1 0))
+  "Returns 0 if x[n+1] contains an odd number of 1-bits, otherwise 1.")
+
+;;; -------------------------------------------------------
+
+(defparameter +LEAST-SIGNIFICANT-BIT+
+  #'(lambda (x[n+1])
+      (declare (integer x[n+1]))
+      (if (logbitp 0 x[n+1]) 1 0))
+  "Extracts the least significant bits from x[n+1].")
+
+;;; -------------------------------------------------------
+
+(defun make-blum-blum-shub-generator (&key p q seed
+                             (output-generator +LEAST-SIGNIFICANT-BIT+))
+  "Creates and returns a nullary function which represents a simple
+   Blum-Blum-Shub pseudorandom number generator with the parameters
+   P, Q, and SEED, returning a list of two elements:
+     (1) the bits selected from the generated number x[n+1] by providing
+         it to the OUTPUT-GENERATOR and obtaining its result,
+     (2) the generated number x[n+1] itself.
+   ---
+   While remaining unchecked, P and Q should be two large primes.
+   SEED should be an integer that is co-prime to M = P * Q, and should
+   not be one or zero.
+   ---
+   The actual output, a sequence of one or more bits, might be changed
+   through the OUTPUT-GENERATOR, which constitutes a function accepting
+   the generated element x[n+1] and returns the output bits, adhering
+   to the signature
+     lambda(x[n+1]) => integer-encoded-random-bits
+   ---
+   Examplary usage:
+     (let ((bbs (make-blum-blum-shub-generator :p 11 :q 19 :seed 3)))
+       (loop repeat 7 do
+         (multiple-value-bind (bit x[n+1]) (funcall bbs)
+           (format T \"~&x[n+1] = ~d => bit = ~d\" x[n+1] bit))))"
+  (let ((M (* p q)))
+    (let ((x0 seed))
+      #'(lambda ()
+          (setf x0 (mod (* x0 x0) M))
+          (let ((output-bits (funcall output-generator x0)))
+            (list x0 output-bits))))))
+
+;;; -------------------------------------------------------
+
+(let ((random-generator (make-blum-blum-shub-generator
+                          :p 11 :q 19 :seed 3
+                          :output-generator +LEAST-SIGNIFICANT-BIT+)))
+  (loop repeat 7 do
+    (print (funcall random-generator))))
+
+;;; -------------------------------------------------------
+
+(let ((random-generator (make-blum-blum-shub-generator
+                          :p 11 :q 19 :seed 3
+                          :output-generator +EVEN-PARITY-BIT+)))
+  (loop repeat 7 do
+    (print (funcall random-generator))))
+
+;;; -------------------------------------------------------
+
+(let ((random-generator (make-blum-blum-shub-generator
+                          :p 11 :q 19 :seed 3
+                          :output-generator +ODD-PARITY-BIT+)))
+  (loop repeat 7 do
+    (print (funcall random-generator))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -- Test 05: Object-oriented design.                             -- ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defstruct (Blum-Blum-Shub-Random-Generator
